f42bb3b1de4a58d8864063c65df8d55913c9067b
QuangSon0409 <buiquangson7782@gmail.com>
2023-07-17 23:36:33 +0700

    update lesson4

--- lesson2/dist/object.js
+++
@@ -1,5 +0,0 @@
-var LEVEl;
-(function (LEVEl) {
-    LEVEl[LEVEl["MANAGER"] = 0] = "MANAGER";
-    LEVEl[LEVEl["STAFF"] = 1] = "STAFF";
-})(LEVEl || (LEVEl = {}));
--- lesson2/dist/primitives.js
+++
@@ -1,9 +0,0 @@
-// string
-var greeting = "Hello world";
-// number
-var num = 10;
-// boolean
-var isCheck = 1 > 2;
-// null/undefined
-var a;
-console.log((a = 10));
